cbd5ba02a5f260c7156d4d3cc85f2975283e0288
Phoenix Zerin <phx@phx.ph>
2019-08-30 00:49:51 +1200

    Dropped support for Python 2.7 👻

--- setup.py
+++ setup.py
@@ -1,15 +1,9 @@
-# coding=utf-8
-# :bc: Not importing unicode_literals because in Python 2 distutils,
-# some values are expected to be byte strings.
-from __future__ import absolute_import, division, print_function
-
 from codecs import StreamReader, open
 from distutils.version import LooseVersion
 from os.path import dirname, join, realpath
 
 from setuptools import setup
 from setuptools.version import __version__
-
 
 ##
 # Because of the way we declare dependencies here, we need a more recent
@@ -18,66 +12,58 @@ from setuptools.version import __version__
 if LooseVersion(__version__) < LooseVersion('20.5'):
     raise EnvironmentError('Django Filters requires setuptools 20.5 or later.')
 
-
 ##
 # Load long description for PyPi.
 readme = join(dirname(realpath(__file__)), 'README.rst')
-with open(readme, 'r', 'utf-8') as f: # type: StreamReader
+with open(readme, 'r', 'utf-8') as f:  # type: StreamReader
     long_description = f.read()
-
 
 ##
 # Off we go!
 setup(
-    name        = 'filters-django',
-    description = 'Adds filters for Django-specific features.',
-    url         = 'https://filters.readthedocs.io/',
+    name='phx-filters-django',
+    description='Adds filters for Django-specific features.',
+    url='https://filters.readthedocs.io/',
 
-    version = '1.0.5',
+    version='2.0.0',
 
-    packages = ['filters_django'],
+    packages=['filters_django'],
 
     # Install package filters into the global registry.
-    entry_points = {
+    entry_points={
         'filters.extensions': [
             'Model = filters_django:Model',
         ],
     },
 
-    long_description = long_description,
+    long_description=long_description,
 
-    install_requires = [
-        # Django 2.0 does not support Python 3.
-        # Good on them, but we have legacy projects to support over here in
-        # filters land (:
-        'Django; python_version >= "3.0"',
-        'Django < 2.0; python_version < "3.0"',
-
-        'filters >= 1.2.2',
-        'six',
+    install_requires=[
+        'Django >= 2.0',
+        'phx-filters',
     ],
 
-    extras_require = {
-        'test-runner': ['detox', 'django-nose'],
+    extras_require={
+        'test-runner': ['tox >= 3.7', 'django-nose'],
     },
 
-    license = 'MIT',
+    license='MIT',
 
-    classifiers = [
+    classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Text Processing :: Filters',
     ],
 
-    keywords = 'data validation, django',
+    keywords='data validation, django',
 
-    author          = 'Phoenix Zerin',
-    author_email    = 'phoenix.zerin@eflglobal.com',
+    author='Phoenix Zerin',
+    author_email='phx@phx.ph',
 )
--- test/filters_django_test.py
+++ test/filters_django_test.py
@@ -1,7 +1,3 @@
-# coding=utf-8
-from __future__ import absolute_import, division, print_function, \
-  unicode_literals
-
 import filters as f
 from filters.test import BaseFilterTestCase
 
@@ -9,194 +5,194 @@ from test.models import Specie
 
 
 class ModelTestCase(BaseFilterTestCase):
-  """
-  Note: most of the animals referenced in this test case are cuddly
-  because, let's face it, those are more fun to research (:
-  """
-  filter_type = f.ext.Model
-
-  def test_pass_none(self):
     """
-    ``None`` always passes this filter.
-    
-    Chain with ``Required`` to reject null values.
+    Note: most of the animals referenced in this test case are cuddly
+    because, let's face it, those are more fun to research (:
     """
-    self.assertFilterPasses(
-      self._filter(None, model=Specie),
-    )
+    filter_type = f.ext.Model
 
-  def test_pass_pk_match(self):
-    """
-    The incoming value matches the PK of an existing record.
-    """
-    # noinspection SpellCheckingInspection
-    target_specie =\
-      Specie.objects.create(
-        binomial_name = 'Mesocricetus Auratus',
-        common_name   = 'Golden Hamster',
-        is_cuddly     = True,
-      )
+    def test_pass_none(self):
+        """
+        ``None`` always passes this filter.
 
-    self.assertFilterPasses(
-      self._filter(target_specie.pk, model=Specie),
-      target_specie,
-    )
+        Chain with ``Required`` to reject null values.
+        """
+        self.assertFilterPasses(
+            self._filter(None, model=Specie),
+        )
 
-  def test_fail_no_pk_match(self):
-    """
-    The incoming value does not match the PK of any existing
-    records.
-    """
-    self.assertFilterErrors(
-      self._filter(42, model=Specie),
-      [f.ext.Model.CODE_NOT_FOUND],
-    )
+    def test_pass_pk_match(self):
+        """
+        The incoming value matches the PK of an existing record.
+        """
+        # noinspection SpellCheckingInspection
+        target_specie = \
+            Specie.objects.create(
+                binomial_name='Mesocricetus Auratus',
+                common_name='Golden Hamster',
+                is_cuddly=True,
+            )
 
-  def test_pass_alternate_field_match(self):
-    """
-    You can configure the constraint to match a value other than
-    PK.
-    """
-    # noinspection SpellCheckingInspection
-    target_specie = Specie.objects.create(
-      binomial_name = 'Setonix Brachyurus',
-      common_name   = 'Quokka',
-      is_cuddly     = False,
-    )
+        self.assertFilterPasses(
+            self._filter(target_specie.pk, model=Specie),
+            target_specie,
+        )
 
-    self.assertFilterPasses(
-      self._filter(
-        target_specie.common_name,
-          model = Specie,
-          field = 'common_name',
-      ),
+    def test_fail_no_pk_match(self):
+        """
+        The incoming value does not match the PK of any existing
+        records.
+        """
+        self.assertFilterErrors(
+            self._filter(42, model=Specie),
+            [f.ext.Model.CODE_NOT_FOUND],
+        )
 
-      target_specie,
-    )
+    def test_pass_alternate_field_match(self):
+        """
+        You can configure the constraint to match a value other than
+        PK.
+        """
+        # noinspection SpellCheckingInspection
+        target_specie = Specie.objects.create(
+            binomial_name='Setonix Brachyurus',
+            common_name='Quokka',
+            is_cuddly=False,
+        )
 
-    # Note that we specified ``common_name`` as the match field, so
-    # PK no longer works.
-    self.assertFilterErrors(
-      self._filter(
-        target_specie.pk,
-          model = Specie,
-          field = 'common_name'
-      ),
+        self.assertFilterPasses(
+            self._filter(
+                target_specie.common_name,
+                model=Specie,
+                field='common_name',
+            ),
 
-      [f.ext.Model.CODE_NOT_FOUND],
-    )
+            target_specie,
+        )
 
-  def test_fail_too_many_matches(self):
-    """
-    The incoming value matches more than one record.
-    """
-    # noinspection SpellCheckingInspection
-    Specie.objects.create(
-      binomial_name = 'Enhydra Lutris',
-      common_name   = 'Sea Otter',
-      is_cuddly     = True,
-    )
+        # Note that we specified ``common_name`` as the match field, so
+        # PK no longer works.
+        self.assertFilterErrors(
+            self._filter(
+                target_specie.pk,
+                model=Specie,
+                field='common_name'
+            ),
 
-    # noinspection SpellCheckingInspection
-    Specie.objects.create(
-      binomial_name = 'Aix Galericulata',
-      common_name   = 'Mandarin Duck',
-      is_cuddly     = True,
-    )
+            [f.ext.Model.CODE_NOT_FOUND],
+        )
 
-    self.assertFilterErrors(
-      self._filter(True, model=Specie, field='is_cuddly'),
-      [f.ext.Model.CODE_NOT_UNIQUE],
-    )
+    def test_fail_too_many_matches(self):
+        """
+        The incoming value matches more than one record.
+        """
+        # noinspection SpellCheckingInspection
+        Specie.objects.create(
+            binomial_name='Enhydra Lutris',
+            common_name='Sea Otter',
+            is_cuddly=True,
+        )
 
-  def test_predicates(self):
-    """
-    You can use ``predicates`` to specify additional filters to add to
-    the query.
-    """
-    # noinspection SpellCheckingInspection
-    target_specie =\
-      Specie.objects.create(
-        binomial_name = 'Bradypus Pygmaeus',
-        common_name   = 'Dwarf Sloth',
-        is_cuddly     = True,
-      )
+        # noinspection SpellCheckingInspection
+        Specie.objects.create(
+            binomial_name='Aix Galericulata',
+            common_name='Mandarin Duck',
+            is_cuddly=True,
+        )
 
-    # Note that without any predicates, the filter will find our specie
-    # just fine.
-    self.assertFilterPasses(
-      self._filter(
-        target_specie.common_name,
-          model = Specie,
-          field = 'common_name',
-      ),
+        self.assertFilterErrors(
+            self._filter(True, model=Specie, field='is_cuddly'),
+            [f.ext.Model.CODE_NOT_UNIQUE],
+        )
 
-      target_specie,
-    )
+    def test_predicates(self):
+        """
+        You can use ``predicates`` to specify additional filters to add to
+        the query.
+        """
+        # noinspection SpellCheckingInspection
+        target_specie = \
+            Specie.objects.create(
+                binomial_name='Bradypus Pygmaeus',
+                common_name='Dwarf Sloth',
+                is_cuddly=True,
+            )
 
-    # However, if we add a predicate that restricts the search to
-    # non-cuddlies, the input is no longer valid.
-    self.assertFilterErrors(
-      self._filter(
-        target_specie.common_name,
-          model   = Specie,
-          field   = 'common_name',
-          filter  = {'is_cuddly': False},
-      ),
+        # Note that without any predicates, the filter will find our specie
+        # just fine.
+        self.assertFilterPasses(
+            self._filter(
+                target_specie.common_name,
+                model=Specie,
+                field='common_name',
+            ),
 
-      [f.ext.Model.CODE_NOT_FOUND],
-    )
+            target_specie,
+        )
 
-  def test_error_predicate_invalid(self):
-    """
-    Predicates must return :py:class:`django.db.models.query.QuerySet`
-    objects, or else they are invalid.
-    """
-    # noinspection SpellCheckingInspection
-    Specie.objects.create(
-      binomial_name = 'Allactaga Tetradactyla',
-      common_name   = 'Jerboa',
+        # However, if we add a predicate that restricts the search to
+        # non-cuddlies, the input is no longer valid.
+        self.assertFilterErrors(
+            self._filter(
+                target_specie.common_name,
+                model=Specie,
+                field='common_name',
+                filter={'is_cuddly': False},
+            ),
 
-      # I honestly can't tell.
-      is_cuddly = None,
-    )
+            [f.ext.Model.CODE_NOT_FOUND],
+        )
 
-    filter_ =\
-      self.filter_type(
-        model = Specie,
-        field = 'common_name',
+    def test_error_predicate_invalid(self):
+        """
+        Predicates must return :py:class:`django.db.models.query.QuerySet`
+        objects, or else they are invalid.
+        """
+        # noinspection SpellCheckingInspection
+        Specie.objects.create(
+            binomial_name='Allactaga Tetradactyla',
+            common_name='Jerboa',
 
-        # ``QuerySet.count`` returns an integer, not a ``QuerySet``.
-        count = (),
-      )
+            # I honestly can't tell.
+            is_cuddly=None,
+        )
 
-    # Note that the exception isn't raised until the filter is applied.
-    with self.assertRaises(ValueError):
-      filter_.apply('Jerboa')
+        filter_ = \
+            self.filter_type(
+                model=Specie,
+                field='common_name',
 
-  def test_pass_predicate_side_effects(self):
-    """
-    Reckless use of predicates that trigger write queries may result in
-    side effects.
-    """
-    # noinspection SpellCheckingInspection
-    target_specie =\
-      Specie.objects.create(
-        binomial_name = 'Otolemur Crassicaudatus',
-        common_name   = 'Galago',
-        is_cuddly     = True,
-      )
+                # ``QuerySet.count`` returns an integer, not a ``QuerySet``.
+                count=(),
+            )
 
-    filter_ = self.filter_type(
-        model = Specie,
-        field = 'binomial_name',
+        # Note that the exception isn't raised until the filter is applied.
+        with self.assertRaises(ValueError):
+            filter_.apply('Jerboa')
 
-        update = {'is_cuddly': False},
-    )
+    def test_pass_predicate_side_effects(self):
+        """
+        Reckless use of predicates that trigger write queries may result in
+        side effects.
+        """
+        # noinspection SpellCheckingInspection
+        target_specie = \
+            Specie.objects.create(
+                binomial_name='Otolemur Crassicaudatus',
+                common_name='Galago',
+                is_cuddly=True,
+            )
 
-    with self.assertRaises(ValueError):
-      filter_.apply(target_specie.binomial_name)
+        filter_ = self.filter_type(
+            model=Specie,
+            field='binomial_name',
 
-    # An exception was raised... but not before the DB record was
-    # updated!
-    self.assertFalse(Specie.objects.get(pk=target_specie.pk).is_cuddly)
+            update={'is_cuddly': False},
+        )
+
+        with self.assertRaises(ValueError):
+            filter_.apply(target_specie.binomial_name)
+
+        # An exception was raised... but not before the DB record was
+        # updated!
+        self.assertFalse(Specie.objects.get(pk=target_specie.pk).is_cuddly)
