53bff3e10ca9df9df84500bd64dfe81088d5803f
Phoenix Zerin <phx@phx.nz>
2023-02-10 14:09:18 +1300

    Big hairy audacious project refresh 🦍
    * Update for latest versions of Python and Django.
    * Fixed an issue preventing test files from getting included in sdist builds.
    * Adopted pyproject.toml standard.
    * Replaced deprecated travis-ci.org build with GitHub Actions.

--- test/settings.py
+++ test/settings.py
@@ -1,21 +1,15 @@
 """
 Minimal Django settings, just enough to get unit tests to run.
 """
-from os.path import join
-from tempfile import mkdtemp
 from uuid import uuid4
-
-BASE_DIR = mkdtemp()
-DEBUG = True
-INSTALLED_APPS = ['django_nose', 'test']
-SECRET_KEY = uuid4().hex
-TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 
 DATABASES = {
     'default': {
-        # You do not need to set up a database server for unit tests;
-        # SQLite will work just fine.
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME':   join(BASE_DIR, 'db.sqlite3'),
-    }
+        'NAME': ':memory:',
+    },
 }
+
+DEBUG = True
+INSTALLED_APPS = ['test.app']
+SECRET_KEY = uuid4().hex
